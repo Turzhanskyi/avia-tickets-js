dfdb79b66e04a4764b1887a19df42924864858c8
Turzhanskyi <turzhansky81@gmail.com>
2020-02-07 02:48:24 +0200

    Serialized date for ticket

--- src/js/store/locations.js
+++ src/js/store/locations.js
@@ -1,13 +1,15 @@
 import api from "../services/apiService";
+import { formatDate } from "../helpers/date";
 
 class Locations {
-  constructor(api) {
+  constructor(api, helpers) {
     this.api = api;
     this.countries = null;
     this.cities = null;
     this.shortCities = {};
     this.lastSearch = {};
     this.airlines = {};
+    this.formatDate = helpers.formatDate;
   }
   async init() {
     const response = await Promise.all([
@@ -94,11 +96,13 @@ class Locations {
         destination_name: this.getCityNameByCode(ticket.destination),
         airline_logo: this.getAirlineLogoByCode(ticket.airline),
         airline_name: this.getAirlineNameByCode(ticket.airline),
-      }
-    })
+        departure_at: this.formatDate(ticket.departure_at, "dd MMM yyy hh:mm"),
+        return_at: this.formatDate(ticket.return_at, "dd MMM yyy hh:mm")
+      };
+    });
   }
 }
 
-const locations = new Locations(api);
+const locations = new Locations(api, { formatDate });
 
 export default locations;
